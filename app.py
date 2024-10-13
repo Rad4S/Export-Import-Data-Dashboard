52b4684238c70cea8fe472cd8d692348994fd813
Rad4S <055035@fsm.ac.in>
2024-10-14 00:13:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
 import streamlit as st
 import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 
 # Load the dataset
